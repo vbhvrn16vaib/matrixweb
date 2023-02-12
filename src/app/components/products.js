6ff2485ee54cfd4cd7c3b6686d5c37eb6985e30f
Vaibhav Rana <vbhvrn@gmail.com>
2023-02-12 12:10:45 +0000

    add matrix sol data

--- src/app/components/products.js
+++ src/app/components/products.js
@@ -1,75 +1,52 @@
 import Image from "next/image";
 import { Col, Container, Row } from "react-bootstrap";
+import ElectricalServices from '../../pages/blog/electrical_services.mdx'
+import PVSYSTEMS from '../../pages/blog/pv_systems.mdx'
+import HAVELSPRODUCT from '../../pages/blog/havell_products.mdx'
+import EHSPOLICY from '../../pages/blog/ehs_policy.mdx'
 
 export default function Products() {
     return <>
-
+        <hr></hr>
         <Container fluid>
             <Row xs={1} md={2}>
+                <Col>
+                    <Image
+                        src="/solar_image.png"
+                        width={500}
+                        height={500} />
+                </Col>
+
                 <Col className="justify-center">
-                    Solar energy products
+                    <PVSYSTEMS />
+                </Col>
+
+            </Row>
+            <hr></hr>
+            <Row>
+                <Col>
+                    <ElectricalServices />
                 </Col>
                 <Col>
                     <Image
-                        src="/solar_image.png"
+                        src="/home-electrical-wiring.webp"
                         width={500}
                         height={500} />
                 </Col>
             </Row>
+            <hr></hr>
             <Row>
-            <Col>
-                    <Image
-                        src="/solar_image.png"
-                        width={500}
-                        height={500} />
-                </Col>
-                <Col>Fire Detection and Alarm System</Col>
-            </Row>
-            <Row>
-                <Col>Fire Suppression Equipments</Col>
+                <div>
+                    <h2>Products</h2>
+                </div>
                 <Col>
-                    <Image
-                        src="/solar_image.png"
-                        width={500}
-                        height={500} />
+                    <HAVELSPRODUCT />
                 </Col>
-            </Row>
-            <Row>
-            <Col>
-                    <Image
-                        src="/solar_image.png"
-                        width={500}
-                        height={500} />
-                </Col>
-                <Col>Solar energy products</Col>
-            </Row>
-            <Row>
-                <Col>CCTV</Col>
                 <Col>
-                    <Image
-                        src="/solar_image.png"
-                        width={500}
-                        height={500} />
+                    <EHSPOLICY />
                 </Col>
-            </Row>
-            <Row>
-            <Col>
-                    <Image
-                        src="/solar_image.png"
-                        width={500}
-                        height={500} />
-                </Col>
-                <Col>Home Automation</Col>
-            </Row>
-            <Row>
-                <Col>Access Control</Col>
-                <Col>
-                    <Image
-                        src="/solar_image.png"
-                        width={500}
-                        height={500} />
-                </Col>
+
             </Row>
         </Container>
-    </>;
+    </>
 }
