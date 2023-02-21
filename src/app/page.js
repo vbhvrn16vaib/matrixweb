5e8d3c1a0e3f5be0ae0d3cb42d65ae41e733b286
vaibhav rana <vbhvrn@gmail.com>
2023-02-21 08:53:39 +0000

    Add excerpts fix sorting (#10)
    * add sorting
    
    * fix heading

--- src/app/page.js
+++ src/app/page.js
@@ -17,8 +17,6 @@ export default function Home() {
 
   return (
     <>
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7962958774331843"
-     crossorigin="anonymous"></script>
     <ParallaxProvider>
       <NavHead />
       <CarouselM />
